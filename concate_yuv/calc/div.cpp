e54806fdc7c30cd2197ea95ac9ff5e5be1d799dc
yuzhu.zhang <yuzhu.zhang@sophgo.com>
2025-01-09 17:51:54 +0800

    modifies

--- concate_yuv/calc/div.cpp
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "head.h"
-
-double div(int a, int b) {
-    return (double)a / b;
-}
--- concate_yuv/del.sh
+++
@@ -1,3 +0,0 @@
-# !/bin/sh
-make clean
-rm -rf CMakeFiles/ cmake_install.cmake CMakeCache.txt Makefile
